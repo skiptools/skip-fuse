3a2a2b0e5b0a30c4f6ec0c3779ff522e72342574
Marc Prud'hommeaux <marc@skip.tools>
2024-12-09 16:36:58 -0500

    Remove SkipModel as a dependency for SkipFuse

--- Package.swift
+++ Package.swift
@@ -9,12 +9,10 @@ let package = Package(
         .library(name: "SkipFuse", type: .dynamic, targets: ["SkipFuse"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip-model.git", from: "1.0.0"),
         .package(url: "https://source.skip.tools/skip-android-bridge.git", "0.0.0"..<"2.0.0"),
     ],
     targets: [
         .target(name: "SkipFuse", dependencies: [
-            .product(name: "SkipModel", package: "skip-model"),
             .product(name: "SkipAndroidBridgeKt", package: "skip-android-bridge"),
         ]),
         .testTarget(name: "SkipFuseTests", dependencies: ["SkipFuse"]),
