8f3295094ad29075730284c5197c7f1d94c0f2d9
Marc Prud'hommeaux <marc@skip.tools>
2025-03-21 09:43:28 -0400

    Add explicit dependency on SwiftJNI to work around linker errors

--- Package.swift
+++ Package.swift
@@ -10,10 +10,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip-android-bridge.git", "0.0.0"..<"2.0.0"),
+        .package(url: "https://source.skip.tools/swift-jni.git", "0.0.0"..<"2.0.0"),
     ],
     targets: [
         .target(name: "SkipFuse", dependencies: [
             .product(name: "SkipAndroidBridge", package: "skip-android-bridge"),
+            // we should be getting this transitively through SkipAndroidBridge, but linker errors arise with "Undefined symbol"
+            .product(name: "SwiftJNI", package: "swift-jni"),
         ]),
         .testTarget(name: "SkipFuseTests", dependencies: ["SkipFuse"]),
     ]
