279a4cc659fcb7f52555b0c75fd58960d8285a29
Marc Prud'hommeaux <marc@skip.tools>
2025-04-01 20:31:44 -0400

    Remove explicit SwiftJNI dependency

--- Package.swift
+++ Package.swift
@@ -15,8 +15,6 @@ let package = Package(
     targets: [
         .target(name: "SkipFuse", dependencies: [
             .product(name: "SkipAndroidBridge", package: "skip-android-bridge"),
-            // we should be getting this transitively through SkipAndroidBridge, but linker errors arise with "Undefined symbol"
-            .product(name: "SwiftJNI", package: "swift-jni"),
         ]),
         .testTarget(name: "SkipFuseTests", dependencies: ["SkipFuse"]),
     ]
